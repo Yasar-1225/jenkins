3223e8fc7da70ec3daa27a293ffc743a7c42a406
Yasar-1225 <yasarj.techzar@gmail.com>
2024-05-28 10:50:16 +0530

    Update HelloWorld.java
    New Changessss

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,8 +13,8 @@
 
 
 public class HelloWorld {
-    public static void (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("sasi hello")
+        
     }
 }
